1618e9ccafd22312af240bb6f83ca0dec7ac6c89
jorgitotk <jorge.calvo@tektonlabs.com>
2017-08-24 11:52:37 -0500

    Add rails admin for models

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
     resources :sessions, only: :create do
       collection do
