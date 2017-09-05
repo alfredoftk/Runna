a68e139cd071c416e49dbfa2d3a45c08b0db8551
jorgitotk <jorge.calvo@tektonlabs.com>
2017-09-05 08:51:55 -0500

    Add login to rails admin

--- app/models/platform_user.rb
+++ app/models/platform_user.rb
@@ -1,5 +1,7 @@
 class PlatformUser < ApplicationRecord
 
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
   validates :description, presence: true
 
 end
