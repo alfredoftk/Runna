57fcd9deb30189fd318b320b08e203dd6c045f31
Josue Velez <josue.velez@tektonlabs.com>
2017-09-05 16:22:55 -0500

    removing proces_company_fiel.rb

--- app/models/process_company_field.rb
+++
@@ -1,6 +0,0 @@
-class ProcessCompanyField < ApplicationRecord
-  belongs_to :employee_process
-  belongs_to :form
-  belongs_to :company_form_field
-  has_one :form_field, through: :company_form_field
-end
